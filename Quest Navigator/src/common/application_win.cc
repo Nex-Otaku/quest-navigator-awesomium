6196cdc2a83b5e71fedb5c24f28d80d5654c83e4
Nex Otaku <nex@otaku.ru>
2013-06-16 13:36:54 +0400

    форматирование

--- Quest Navigator/src/common/application_win.cc
+++ Quest Navigator/src/common/application_win.cc
@@ -6,61 +6,61 @@
 using namespace Awesomium;
 
 class ApplicationWin : public Application {
-  bool is_running_;
- public:
-  ApplicationWin() {
-    is_running_ = true;
-    listener_ = NULL;
-    web_core_ = NULL;
-  }
+	bool is_running_;
+public:
+	ApplicationWin() {
+		is_running_ = true;
+		listener_ = NULL;
+		web_core_ = NULL;
+	}
 
-  virtual ~ApplicationWin() {
-    if (listener())
-      listener()->OnShutdown();
+	virtual ~ApplicationWin() {
+		if (listener())
+			listener()->OnShutdown();
 
-    if (web_core_)
-      web_core_->Shutdown();
-  }
+		if (web_core_)
+			web_core_->Shutdown();
+	}
 
-  virtual void Run() {
-    Load();
+	virtual void Run() {
+		Load();
 
-    // Main message loop:
-    MSG msg;
-    while (GetMessage(&msg, NULL, 0, 0) && is_running_) {
-      web_core_->Update();
-      TranslateMessage(&msg);
-      DispatchMessage(&msg);
-      if (listener())
-        listener()->OnUpdate();
-    }
-  }
-  
-  virtual void Quit() {
-    is_running_ = false;
-  }
+		// Main message loop:
+		MSG msg;
+		while (GetMessage(&msg, NULL, 0, 0) && is_running_) {
+			web_core_->Update();
+			TranslateMessage(&msg);
+			DispatchMessage(&msg);
+			if (listener())
+				listener()->OnUpdate();
+		}
+	}
 
-  virtual void Load() {
-     web_core_ = WebCore::Initialize(WebConfig());
+	virtual void Quit() {
+		is_running_ = false;
+	}
 
-    if (listener())
-      listener()->OnLoaded();
-  }
+	virtual void Load() {
+		web_core_ = WebCore::Initialize(WebConfig());
 
-  virtual View* CreateView(int width, int height) {
-    return View::Create(width, height);
-  }
+		if (listener())
+			listener()->OnLoaded();
+	}
 
-  virtual void DestroyView(View* view) {
-    delete view;
-  }
+	virtual View* CreateView(int width, int height) {
+		return View::Create(width, height);
+	}
 
-  virtual void ShowMessage(const char* message) {
-    std::wstring message_str(message, message + strlen(message));
-    MessageBox(0, message_str.c_str(), message_str.c_str(), NULL);
-  }
+	virtual void DestroyView(View* view) {
+		delete view;
+	}
+
+	virtual void ShowMessage(const char* message) {
+		std::wstring message_str(message, message + strlen(message));
+		MessageBox(0, message_str.c_str(), message_str.c_str(), NULL);
+	}
 };
 
 Application* Application::Create() {
-  return new ApplicationWin();
+	return new ApplicationWin();
 }
