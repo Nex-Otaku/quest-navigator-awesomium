f9fcc05e6da537787623a534c38022f97dc0a904
Nex Otaku <nex@otaku.ru>
2014-03-31 12:57:17 +0400

    скин для игры по умолчанию

---
+++ Quest Navigator/installer/assets_files/assets/standalone_content/skins/js/game.js
@@ -0,0 +1,173 @@
+/* Функции скина для игры */
+
+//var skin
+
+function qspSkinOnInitApi() {
+//QspLib.alert('asd');
+	// Для мобильников в портретном режиме отключаем обычные скроллы.
+	// Потестить на девайсах, может и не пригодится.
+	if ($(window).width() <= 480) {
+	/*
+		if (qsp_iScroll_main != null) {
+			qsp_iScroll_main.destroy();
+			qsp_iScroll_main = null;
+		}
+		if (qsp_iScroll_acts != null) {
+			qsp_iScroll_acts.destroy();
+			qsp_iScroll_acts = null;
+		}
+		if (qsp_iScroll_vars != null) {
+			qsp_iScroll_vars.destroy();
+			qsp_iScroll_vars = null;
+		}
+		if (qsp_iScroll_objs != null) {
+			qsp_iScroll_objs.destroy();
+			qsp_iScroll_objs = null;
+		}
+		*/
+	}
+
+	
+	
+	
+//****************************************************************************************	
+//****************************************************************************************	
+//****************************************************************************************	
+	// // Закрываем инвентарь по клику
+	// setTimeout( function() { // Delay for Mozilla
+			// $("#qsp-inv").bind('click', qspHandlerInvClick);
+			// $("#qsp-wrapper-objs").bind('click', qspHandlerInvClick);
+			// $("#qsp-user2 .qsp-skin-overlay").bind('click', qspHandlerInvClick);
+	// }, 0);
+//****************************************************************************************	
+//****************************************************************************************	
+//****************************************************************************************	
+	
+	
+}
+
+
+/* Собственные функции скина */ 
+function skinToggleInv() {
+	$("#qsp-inv").slideToggle();
+	$("#skin-inv-toggle").toggleClass('open');
+}
+
+
+/* Функции скина для игры */
+
+// Колбэки
+
+function qspSkinOnDeviceSet() {
+	// Вызывается, когда мы узнали, на каком устройстве запущена игра
+	var more_games_link = 'http://qsp.su';
+	if (qspIsAndroid) {
+		more_games_link = 'market://search?q=pub:Butterfly+Lantern';
+	} else if (qspIsIos) {
+		more_games_link = 'itms-apps://itunes.apple.com/ru/artist/butterfly-lantern-interactive/id508671395';
+	}
+	$("#more-games-button a").attr('href', more_games_link);
+}
+
+function qspSkinOnSetGroupedContent() {
+    skinRefreshBugfix();
+	
+	// При первом вызове список пуст.
+	// После первого заполнения список уже не пуст, 
+	// поэтому вызов выполняться не будет.
+	if (qspLocalGames === null) {
+		// Запрашиваем список локальных игр.
+		// Когда ответ будет получен, список заполнится автоматически.
+		QspLib.listLocalGames();
+	}
+}
+
+function qspSkinOnSave() {
+	//skinHideInv();
+}
+
+function qspSkinOnLoad() {
+	//skinHideInv();
+}
+
+function qspSkinOnRestart() {
+	//skinHideInv();
+}
+
+// Свои функции
+
+var skinHideScrollsOriginal = 0;
+var skinInv = false;
+var skinMusic = true;
+var skinStage = "";
+
+//var qspHandlerInvClick = function() { skinHideInv(); };
+
+function skinToggleMusic() {
+	skinMusic = !skinMusic;
+	skinSetMusicButton();
+	QspLib.setMute(!skinMusic);
+}
+
+// function skinHideInv() {
+	// // прячем инвентарь
+	// if (skinInv) {
+		// skinInv = false;
+		// skinMoveInv();
+		// skinHideScrolls(true);
+	// }
+// }
+
+// function skinShowInv() {
+	// skinInv = true;
+	// skinMoveInv();
+	// skinHideScrolls(true);
+// }
+
+function skinHideScrolls(immediate) {
+	qspGameSkin.hideScrollMain = ((skinHideScrollsOriginal === 1) || skinInv) ? 1 : 0;
+	if (immediate) {
+		qspApplyScrollsVisibility();
+	}
+}
+
+// function skinmoveinv() {
+	// $('#qsp-wrapper-objs').toggle(skininv);
+	// $('#qsp-user2').toggle(skininv);
+	// qsprefreshobjsscroll();
+// }
+
+function skinSetMusicButton() {
+	skinToggleButton('#qsp-user-music img', '(button_music_)(on|off)(_pressed)?', '$1' + (skinMusic ? 'on' : 'off') + '$3');
+}
+
+function skinSetStage(cssClass) {
+	// Переключаем класс всего body, тем самым задаем разный стиль для разных игровых экранов
+	var t = $(document.body);
+	if ((skinStage !== '') && (t.hasClass(skinStage))) {
+		t.removeClass(skinStage);
+	}
+	skinStage = cssClass;
+	if ((cssClass !== '') && (!t.hasClass(cssClass))) {
+		t.addClass(cssClass);
+	}
+}
+
+function skinToggleButton(selector, pattern, replacement) {
+	var t = $(selector);
+	if (t.length == 0)
+		return;
+	var re = new RegExp(pattern, "g");
+	var btn1 = t.attr('src').replace(re, replacement);
+	var btn2 = t.attr('data-pressed').replace(re, replacement);
+	t.attr('src', btn1);
+	t.attr('data-pressed', btn2);
+}
+
+function skinRefreshBugfix()
+{
+	// Показываем и сразу скрываем невидимый блок размером с экран.
+	// Без этого в эмуляторе не обновляется описание.
+	$('#qsp-refresh-bugfix').show();
+	$('#qsp-refresh-bugfix').hide();
+}
