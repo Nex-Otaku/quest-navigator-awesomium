2d22304a540ef934cf3eedae3efee2d23d775df5
Nex Otaku <nex@otaku.ru>
2014-08-10 21:32:09 +0400

    переключил на тестирование

--- Quest Navigator/src/quest-navigator/utils.h
+++ Quest Navigator/src/quest-navigator/utils.h
@@ -28,7 +28,7 @@ namespace QuestNavigator {
 	static const string QN_WINDOWS_UPDATE_FEED = "http://feeds.feedburner.com/text-games/quest-navigator-windows";
 	static const string QN_COMPANY_NAME = "QSP";
 	static const string QN_APP_NAME = "Quest Navigator";
-	static const string QN_VERSION = "0.0.22";
+	static const string QN_VERSION = "0.0.23-test";
 	static const string QN_APP_GUID = "95b90169-f70d-4269-b982-d9c3038b8348";
 	static const wchar_t szWindowClass[] = L"QnViewWinClass";
 	static const wchar_t szLogWindowClass[] = L"QnLogWinClass";
